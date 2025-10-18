4d92db3bc19a1956628fab5570fde992c303eba3
hamidss278 <hamidss278@gmail.com>
2025-10-18 15:20:52 +0330

    autoDoor.cs

--- autoDoor.cs
+++ autoDoor.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class autoDoor : MonoBehaviour
 {
-    public Animator anim;
+    public Animator anim; //animation you make in game
 
 
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player")) // make sure to change the player tag to Player
         {
             anim.SetTrigger("open");
         }
-        if (other.CompareTag("Ai"))
+        if (other.CompareTag("Ai")) // if you had an ai in your game this will work i recommend to not touch it
         {
             anim.SetTrigger("open");
         }
